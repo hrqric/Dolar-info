bf75f48a42360839b395cdab805ca648b1f49523
Cauê Henrique Ricardo <cricardo@chemyunion.local>
2025-03-18 14:15:14 -0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
+let urlXML = "https://economia.awesomeapi.com.br/xml/available/uniq";
+
 const getAPI = 'https://economia.awesomeapi.com.br/last/'
 const value1 = 'USD'
 
-const htmlvalorIn = '1'; // criar input pra valor
+const inp_valorIn = document.querySelector('.valorin'); // criar input pra valor
+const cb_cointype = document.querySelector('.coin')
 const htmlValorOut =  document.querySelector('.valorout')
 
 const getValue = async (moeda) => {
@@ -13,15 +16,86 @@ const getValue = async (moeda) => {
         return data
     }
     else {
-        console.log('erro')
+        console.log('erro');
     }
 }
 
 const renderValue = async (moeda) => {
     const data = await getValue(moeda);
+    const conv = Object.keys(data)[0]
+    const inp = parseFloat(inp_valorIn.value)
+    const valor = parseFloat(data[conv].bid)
+    console.log(conv)
+    console.log(`input ${inp}`)
     if (data) {
-        htmlValorOut.innerHTML = data.USDBRL.bid
+        if (inp > 1) {
+            htmlValorOut.innerHTML =  (valor * inp).toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'BRL',
+            });
+
+            console.log(htmlValorOut.innerHTML)
     }
+        else{
+            htmlValorOut.innerHTML = valor.toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'BRL',
+            })
+        }
+}
 }
 
-renderValue(value1) // criar input pra valor
+const getXML = async (urlXML) => {
+    $.ajax(urlXML)
+    .done(function(xml){
+        $(xml).find('xml').children().each(function(){
+            const defaultCode = 'USD'
+            const codigo = this.tagName;
+            const nome = $(this).text();
+            console.log(`${codigo} - ${nome}`);
+
+            if(codigo == defaultCode){
+                $('.coin').append(
+                    $('<option>', {
+                        value: codigo,
+                        text: `${codigo} - ${nome}`,
+                        selected: true
+                    }));
+            }
+
+            else{
+                $('.coin').append(
+                    $('<option>', {
+                        value: codigo,
+                        text: `${codigo} - ${nome}`
+                    }));
+            }
+
+        });
+    })
+    
+    .fail(function(){
+        alert("Erro na leitura")
+    })
+}
+
+inp_valorIn.addEventListener('change', function(){
+    if (this.value.trim() === ''){
+        this.value = '1'
+    }
+})
+
+
+cb_cointype.addEventListener("change", (event) => {
+    event.preventDefault();
+    renderValue(cb_cointype.value.toUpperCase());
+})
+
+inp_valorIn.addEventListener("change", (event) => {
+    event.preventDefault();
+    renderValue(cb_cointype.value)
+})
+
+
+getXML(urlXML)
+renderValue(value1) 
